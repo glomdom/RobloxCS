5c7fae4fe39858e806107b5a9ea30336c69a7c8f
Kiril S. <22115@uktc-bg.com>
2025-08-02 01:43:03 +0300

    feat: add bool types

---
+++ RobloxCS.AST/Types/BooleanTypeInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace RobloxCS.AST.Types;
+
+public sealed class BooleanTypeInfo : TypeInfo {
+    public required bool Value { get; set; }
+    
+    public static BooleanTypeInfo FromBoolean(bool value) => new() { Value = value };
+}
