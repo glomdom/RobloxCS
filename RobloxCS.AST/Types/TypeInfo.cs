52c9dd4c38e472aad4600a9bbb325b5100762240
Kiril S. <22115@uktc-bg.com>
2025-08-02 01:44:07 +0300

    docs: add todo for types

--- RobloxCS.AST/Types/TypeInfo.cs
+++ RobloxCS.AST/Types/TypeInfo.cs
@@ -1,5 +1,7 @@
 ﻿namespace RobloxCS.AST.Types;
 
+// TODO: Callback, Generic, GenericPack, Intersection, Module, Optional, Table, Typeof, Tuple, Union, Variadic, VariadicPack
+
 /// <summary>
 /// Any type, such as <c>string</c>, <c>boolean?</c>, <c>number | boolean</c>, etc.
 /// </summary>
