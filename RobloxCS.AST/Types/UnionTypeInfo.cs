94a2acd4ef736ee288439a9daaf463e25b98a53a
Kiril S. <22115@uktc-bg.com>
2025-08-05 21:34:13 +0300

    feat: union types

---
+++ RobloxCS.AST/Types/UnionTypeInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace RobloxCS.AST.Types;
+
+public sealed class UnionTypeInfo : TypeInfo {
+    public required List<TypeInfo> Types;
+
+    public static UnionTypeInfo FromTypes(List<TypeInfo> types) => new() { Types = types };
+    public static UnionTypeInfo FromInfos(params TypeInfo[] types) => new() { Types = [..types] };
+
+    public static UnionTypeInfo FromNames(params string[] typeNames) => new() {
+        Types = typeNames.Select(BasicTypeInfo.FromString).Cast<TypeInfo>().ToList()
+    };
+}
