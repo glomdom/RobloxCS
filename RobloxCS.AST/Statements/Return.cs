4b7e60be7c3cbbd8fc2e0a73839131d8b877a860
Kiril S. <22115@uktc-bg.com>
2025-08-08 17:22:12 +0300

    feat: `Return` helpers

--- RobloxCS.AST/Statements/Return.cs
+++ RobloxCS.AST/Statements/Return.cs
@@ -3,5 +3,8 @@
 public sealed class Return : Statement {
     public required List<Expression> Returns;
 
+    public static Return FromExpressions(List<Expression> expressions) => new() { Returns = expressions };
+    public static Return Empty() => new() { Returns = [] };
+
     public override Return DeepClone() => new() { Returns = Returns.Select(ret => (Expression)ret.DeepClone()).ToList() };
 }
