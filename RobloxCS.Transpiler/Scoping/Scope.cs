6800337f10549c5ead497abdd30a7a0830457f85
glomdom <sadonkovkiril@gmail.com>
2025-08-16 02:31:49 +0300

    feat: basic scoping sketch

---
+++ RobloxCS.Transpiler/Scoping/Scope.cs
@@ -0,0 +1,5 @@
+﻿namespace RobloxCS.Transpiler.Scoping;
+
+public struct Scope<T>(Action<T> setter, T previous) : IDisposable {
+    public void Dispose() => setter(previous);
+}
---
+++ RobloxCS.Transpiler/Scoping/ScopeHelpers.cs
@@ -0,0 +1,10 @@
+﻿namespace RobloxCS.Transpiler.Scoping;
+
+public static class ScopeHelpers {
+    public static IDisposable Swap<T>(ref T target, T next, Action<T> setter) {
+        var prev = target;
+        setter(next);
+
+        return new Scope<T>(setter, prev);
+    }
+}
