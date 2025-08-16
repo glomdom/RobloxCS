5d0ad41edcefe3418aaf93e83cde0b68630f42da
glomdom <sadonkovkiril@gmail.com>
2025-08-16 04:41:30 +0300

    chore: silence warnings

--- RobloxCS.Example/Class1.cs
+++ RobloxCS.Example/Class1.cs
@@ -2,9 +2,9 @@
 
 internal class Program {
     public int A, B;
-    public readonly float ReadonlyFloat;
-    public readonly string ReadonlyString;
-    public readonly int ReadonlyNumber;
+    public readonly float ReadonlyFloat = 0.0f;
+    public readonly string ReadonlyString = "Hello, World!";
+    public readonly int ReadonlyNumber = 2;
 
     internal Program(int a, int b) {
         A = a;
