c2ef336219375d1e9314bb7279781d76455a1684
glomdom <sadonkovkiril@gmail.com>
2025-08-20 12:42:35 +0300

    feat: ast visitor interfaces

--- RobloxCS.AST/AstNode.cs
+++ RobloxCS.AST/AstNode.cs
@@ -2,4 +2,7 @@
 
 public abstract class AstNode {
     public abstract AstNode DeepClone();
+
+    public abstract void Accept(IAstVisitor visitor);
+    public abstract T Accept<T>(IAstVisitor<T> visitor);
 }
