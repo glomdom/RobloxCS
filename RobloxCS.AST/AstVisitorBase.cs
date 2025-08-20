3c913910bc68d1a76c9f297cfcd79ea02a17d5c4
glomdom <sadonkovkiril@gmail.com>
2025-08-20 16:37:19 +0300

    feat: sane defaults

---
+++ RobloxCS.AST/AstVisitorBase.cs
@@ -0,0 +1,61 @@
+﻿using RobloxCS.AST.Expressions;
+using RobloxCS.AST.Generics;
+using RobloxCS.AST.Prefixes;
+using RobloxCS.AST.Statements;
+using RobloxCS.AST.Suffixes;
+using RobloxCS.AST.Types;
+
+namespace RobloxCS.AST;
+
+public class AstVisitorBase : IAstVisitor {
+    public virtual void DefaultVisit(AstNode node) { }
+    
+    public virtual void Visit(Block node) => DefaultVisit(node);
+    public virtual void Visit(Expression node) => DefaultVisit(node);
+    public virtual void Visit(FunctionArgs node) => DefaultVisit(node);
+    public virtual void Visit(FunctionBody node) => DefaultVisit(node);
+    public virtual void Visit(Parameter node) => DefaultVisit(node);
+    public virtual void Visit(Prefix node) => DefaultVisit(node);
+    public virtual void Visit(Statement node) => DefaultVisit(node);
+    public virtual void Visit(Suffix node) => DefaultVisit(node);
+    public virtual void Visit(Var node) => DefaultVisit(node);
+    
+    public virtual void Visit(AnonymousFunction node) => DefaultVisit(node);
+    public virtual void Visit(BooleanExpression node) => DefaultVisit(node);
+    public virtual void Visit(FunctionCall node) => DefaultVisit(node);
+    public virtual void Visit(NumberExpression node) => DefaultVisit(node);
+    public virtual void Visit(StringExpression node) => DefaultVisit(node);
+    public virtual void Visit(SymbolExpression node) => DefaultVisit(node);
+    public virtual void Visit(TableConstructor node) => DefaultVisit(node);
+    public virtual void Visit(TableField node) => DefaultVisit(node);
+    public virtual void Visit(NoKey node) => DefaultVisit(node);
+    public virtual void Visit(NameKey node) => DefaultVisit(node);
+
+    public virtual void Visit(GenericDeclaration node) => DefaultVisit(node);
+    public virtual void Visit(GenericDeclarationParameter node) => DefaultVisit(node);
+    public virtual void Visit(GenericParameterInfo node) => DefaultVisit(node);
+
+    public virtual void Visit(ExpressionPrefix node) => DefaultVisit(node);
+    public virtual void Visit(NamePrefix node) => DefaultVisit(node);
+
+    public virtual void Visit(Assignment node) => DefaultVisit(node);
+    public virtual void Visit(DoStatement node) => DefaultVisit(node);
+    public virtual void Visit(LocalAssignment node) => DefaultVisit(node);
+    public virtual void Visit(Return node) => DefaultVisit(node);
+
+    public virtual void Visit(AnonymousCall node) => DefaultVisit(node);
+    public virtual void Visit(Call node) => DefaultVisit(node);
+
+    public virtual void Visit(ArrayTypeInfo node) => DefaultVisit(node);
+    public virtual void Visit(BasicTypeInfo node) => DefaultVisit(node);
+    public virtual void Visit(BooleanTypeInfo node) => DefaultVisit(node);
+    public virtual void Visit(CallbackTypeInfo node) => DefaultVisit(node);
+    public virtual void Visit(IntersectionTypeInfo node) => DefaultVisit(node);
+    public virtual void Visit(StringTypeInfo node) => DefaultVisit(node);
+    public virtual void Visit(TableTypeInfo node) => DefaultVisit(node);
+    public virtual void Visit(TypeArgument node) => DefaultVisit(node);
+    public virtual void Visit(TypeDeclaration node) => DefaultVisit(node);
+    public virtual void Visit(TypeField node) => DefaultVisit(node);
+    public virtual void Visit(TypeFieldKey node) => DefaultVisit(node);
+    public virtual void Visit(UnionTypeInfo node) => DefaultVisit(node);
+}
---
+++ RobloxCS.AST/AstVisitorBaseT.cs
@@ -0,0 +1,61 @@
+﻿using RobloxCS.AST.Expressions;
+using RobloxCS.AST.Generics;
+using RobloxCS.AST.Prefixes;
+using RobloxCS.AST.Statements;
+using RobloxCS.AST.Suffixes;
+using RobloxCS.AST.Types;
+
+namespace RobloxCS.AST;
+
+public class AstVisitorBase<T> : IAstVisitor<T> where T : AstNode {
+    public virtual T DefaultVisit(AstNode node) => null!;
+    
+    public virtual T Visit(Block node) => DefaultVisit(node);
+    public virtual T Visit(Expression node) => DefaultVisit(node);
+    public virtual T Visit(FunctionArgs node) => DefaultVisit(node);
+    public virtual T Visit(FunctionBody node) => DefaultVisit(node);
+    public virtual T Visit(Parameter node) => DefaultVisit(node);
+    public virtual T Visit(Prefix node) => DefaultVisit(node);
+    public virtual T Visit(Statement node) => DefaultVisit(node);
+    public virtual T Visit(Suffix node) => DefaultVisit(node);
+    public virtual T Visit(Var node) => DefaultVisit(node);
+    
+    public virtual T Visit(AnonymousFunction node) => DefaultVisit(node);
+    public virtual T Visit(BooleanExpression node) => DefaultVisit(node);
+    public virtual T Visit(FunctionCall node) => DefaultVisit(node);
+    public virtual T Visit(NumberExpression node) => DefaultVisit(node);
+    public virtual T Visit(StringExpression node) => DefaultVisit(node);
+    public virtual T Visit(SymbolExpression node) => DefaultVisit(node);
+    public virtual T Visit(TableConstructor node) => DefaultVisit(node);
+    public virtual T Visit(TableField node) => DefaultVisit(node);
+    public virtual T Visit(NoKey node) => DefaultVisit(node);
+    public virtual T Visit(NameKey node) => DefaultVisit(node);
+
+    public virtual T Visit(GenericDeclaration node) => DefaultVisit(node);
+    public virtual T Visit(GenericDeclarationParameter node) => DefaultVisit(node);
+    public virtual T Visit(GenericParameterInfo node) => DefaultVisit(node);
+
+    public virtual T Visit(ExpressionPrefix node) => DefaultVisit(node);
+    public virtual T Visit(NamePrefix node) => DefaultVisit(node);
+
+    public virtual T Visit(Assignment node) => DefaultVisit(node);
+    public virtual T Visit(DoStatement node) => DefaultVisit(node);
+    public virtual T Visit(LocalAssignment node) => DefaultVisit(node);
+    public virtual T Visit(Return node) => DefaultVisit(node);
+
+    public virtual T Visit(AnonymousCall node) => DefaultVisit(node);
+    public virtual T Visit(Call node) => DefaultVisit(node);
+
+    public virtual T Visit(ArrayTypeInfo node) => DefaultVisit(node);
+    public virtual T Visit(BasicTypeInfo node) => DefaultVisit(node);
+    public virtual T Visit(BooleanTypeInfo node) => DefaultVisit(node);
+    public virtual T Visit(CallbackTypeInfo node) => DefaultVisit(node);
+    public virtual T Visit(IntersectionTypeInfo node) => DefaultVisit(node);
+    public virtual T Visit(StringTypeInfo node) => DefaultVisit(node);
+    public virtual T Visit(TableTypeInfo node) => DefaultVisit(node);
+    public virtual T Visit(TypeArgument node) => DefaultVisit(node);
+    public virtual T Visit(TypeDeclaration node) => DefaultVisit(node);
+    public virtual T Visit(TypeField node) => DefaultVisit(node);
+    public virtual T Visit(TypeFieldKey node) => DefaultVisit(node);
+    public virtual T Visit(UnionTypeInfo node) => DefaultVisit(node);
+}
