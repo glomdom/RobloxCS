132e354c83d51e1d6745d46efbd9d3ce791efaf0
glomdom <sadonkovkiril@gmail.com>
2025-08-20 21:44:52 +0300

    feat: chunk & function declaration

---
+++ RobloxCS.AST/Statements/FunctionDeclaration.cs
@@ -0,0 +1,15 @@
+﻿namespace RobloxCS.AST.Statements;
+
+public sealed class FunctionDeclaration : Statement {
+    public required string Name { get; set; }
+    public required FunctionBody Body { get; set; }
+
+    public override FunctionDeclaration DeepClone() => new() { Name = Name, Body = Body.DeepClone() };
+
+    public override void Accept(IAstVisitor v) => v.VisitFunctionDeclaration(this);
+    public override T Accept<T>(IAstVisitor<T> v) => v.VisitFunctionDeclaration(this);
+
+    public override IEnumerable<AstNode> Children() {
+        yield return Body;
+    }
+}
