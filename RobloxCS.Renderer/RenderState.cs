800895f43e2b3dce4300184934b82d857c63f3c7
glomdom <sadonkovkiril@gmail.com>
2025-08-20 23:16:19 +0300

    feat(render): get rendering to work to a certain point

--- RobloxCS.Renderer/RenderState.cs
+++ RobloxCS.Renderer/RenderState.cs
@@ -1,37 +1,10 @@
-﻿using System.Collections.Immutable;
-using System.Text;
-using RobloxCS.AST;
-using RobloxCS.AST.Expressions;
-using RobloxCS.AST.Prefixes;
-using RobloxCS.AST.Statements;
-using RobloxCS.AST.Suffixes;
-using RobloxCS.AST.Types;
-using RobloxCS.Renderer.Renderers;
+﻿using System.Text;
 
 namespace RobloxCS.Renderer;
 
 public class RenderState {
     public string Indent => new(' ', _indentLevel * IndentCharacter.Length);
     public StringBuilder Builder = new();
-
-    private ImmutableDictionary<Type, IRenderer> Renderers { get; } = ImmutableDictionary.CreateRange(
-        [
-            AdapterForNode<LocalAssignment, LocalAssignmentRenderer>(),
-            AdapterForNode<Block, BlockRenderer>(),
-            AdapterForNode<Return, ReturnRenderer>(),
-            AdapterForNode<DoStatement, DoRenderer>(),
-            AdapterForNode<Assignment, AssignmentRenderer>(),
-            AdapterForNode<Var, VarRenderer>(),
-            AdapterForNode<Expression, ExpressionRenderer>(),
-            AdapterForNode<TypeDeclaration, TypeDeclarationRenderer>(),
-            AdapterForNode<TypeInfo, TypeInfoRenderer>(),
-            AdapterForNode<TypeFieldKey, TypeFieldKeyRenderer>(),
-            AdapterForNode<TypeArgument, TypeArgumentRenderer>(),
-            AdapterForNode<Prefix, PrefixRenderer>(),
-            AdapterForNode<Suffix, SuffixRenderer>(),
-            AdapterForNode<Parameter, ParameterRenderer>(),
-        ]
-    );
 
     private const string IndentCharacter = "  ";
     private int _indentLevel;
@@ -41,66 +14,4 @@ public class RenderState {
     public void AppendIndent() => Builder.Append(Indent);
     public void AppendIndented(string text) => Builder.Append($"{Indent}{text}");
     public void AppendIndentedLine(string text) => Builder.AppendLine($"{Indent}{text}");
-
-    public void RenderPunctuated<T>(IList<T> list, RenderState state) where T : AstNode {
-        for (var i = 0; i < list.Count; i++) {
-            var renderer = GetRenderer<T>();
-            renderer.Render(state, list[i]);
-
-            if (i != list.Count - 1) {
-                state.Builder.Append(", ");
-            }
-        }
-    }
-
-    /// <summary>
-    /// Renders a list of nodes separated by the provided <paramref name="delimiter"/>.
-    /// </summary>
-    /// <param name="list">A list containing <see cref="AstNode"/>s.</param>
-    /// <param name="state">The shared render state.</param>
-    /// <param name="delimiter">The delimiter to separate nodes by. Automatically surrounded with spaces.</param>
-    public void RenderDelimited<T>(IList<T> list, RenderState state, char delimiter) where T : AstNode {
-        for (var i = 0; i < list.Count; i++) {
-            var renderer = GetRenderer<T>();
-            renderer.Render(state, list[i]);
-
-            if (i != list.Count - 1) {
-                state.Builder.Append($" {delimiter} ");
-            }
-        }
-    }
-
-    /// <summary>
-    /// Gets a renderer from the provided <c>type</c>.
-    /// </summary>
-    /// <returns>A renderer, if it was found.</returns>
-    /// <exception cref="RendererNotFoundException">Thrown when a renderer was not found.</exception>
-    public IRenderer GetRenderer(Type type) {
-        if (!Renderers.TryGetValue(type, out var renderer)) {
-            throw new RendererNotFoundException("Renderer is not registered/implemented.", type.Name);
-        }
-
-        return renderer;
-    }
-
-    /// <summary>
-    /// Gets a renderer from the provided <c>type</c>.
-    /// </summary>
-    /// <returns>A typed renderer, if it was found.</returns>
-    /// <exception cref="RendererNotFoundException">Thrown when a renderer was not found.</exception>
-    /// <exception cref="InvalidCastException">Thrown if a renderer was found, but does not match type constraint.</exception>
-    public IRenderer<T> GetRenderer<T>() where T : AstNode {
-        var renderer = GetRenderer(typeof(T));
-        if (renderer is IRenderer<T> tRenderer) {
-            return tRenderer;
-        }
-
-        // this shouldnt happen but better safe than sorry. TODO: probably add a catch for this somewhere
-        throw new InvalidCastException($"Renderer for type {typeof(T).Name} is not of expected type IRenderer<{typeof(T).Name}>.");
-    }
-
-    private static KeyValuePair<Type, IRenderer> AdapterForNode<TNode, TRenderer>()
-        where TNode : AstNode where TRenderer : IRenderer<TNode>, new() {
-        return new KeyValuePair<Type, IRenderer>(typeof(TNode), new RendererAdapter<TNode>(new TRenderer()));
-    }
 }
--- RobloxCS.Renderer/RendererWalker.cs
+++ RobloxCS.Renderer/RendererWalker.cs
@@ -1,7 +1,232 @@
 ﻿using RobloxCS.AST;
+using RobloxCS.AST.Expressions;
+using RobloxCS.AST.Prefixes;
 using RobloxCS.AST.Statements;
+using RobloxCS.AST.Suffixes;
 using RobloxCS.AST.Types;
 
 namespace RobloxCS.Renderer;
 
-public class RendererWalker : AstVisitorBase { }
+public class RendererWalker : AstVisitorBase {
+    private readonly RenderState _state = new();
+
+    public string Render(Chunk chunk) {
+        chunk.Accept(this);
+
+        return _state.Builder.ToString();
+    }
+
+    public override void VisitTypeDeclaration(TypeDeclaration node) {
+        _state.AppendIndent();
+        _state.Builder.Append("type ");
+        _state.Builder.Append(node.Name);
+        _state.Builder.Append(" = ");
+
+        node.DeclareAs.Accept(this);
+
+        _state.Builder.AppendLine();
+    }
+
+    public override void VisitTableTypeInfo(TableTypeInfo node) {
+        _state.Builder.Append('{');
+        _state.Builder.AppendLine();
+        _state.PushIndent();
+
+        foreach (var field in node.Fields) {
+            _state.AppendIndent();
+            field.Accept(this);
+            _state.Builder.AppendLine(",");
+        }
+
+        _state.PopIndent();
+        _state.Builder.Append('}');
+    }
+
+    public override void VisitTypeField(TypeField node) {
+        node.Key.Accept(this);
+        _state.Builder.Append(": ");
+        node.Value.Accept(this);
+    }
+
+    public override void VisitNameTypeFieldKey(NameTypeFieldKey node) {
+        _state.Builder.Append(node.Name);
+    }
+
+    public override void VisitCallbackTypeInfo(CallbackTypeInfo node) {
+        _state.Builder.Append('(');
+
+        RenderDelimited(node.Arguments, ", ");
+
+        _state.Builder.Append(')');
+        _state.Builder.Append(" -> ");
+        node.ReturnType.Accept(this);
+    }
+
+    public override void VisitBasicTypeInfo(BasicTypeInfo node) {
+        _state.Builder.Append(node.Name);
+    }
+
+    public override void VisitTypeArgument(TypeArgument node) {
+        _state.Builder.Append(node.Name);
+        _state.Builder.Append(": ");
+        node.TypeInfo.Accept(this);
+    }
+
+    public override void VisitLocalAssignment(LocalAssignment node) {
+        _state.AppendIndent();
+        _state.Builder.Append("local ");
+
+        for (var i = 0; i < node.Names.Count; i++) {
+            node.Names[i].Accept(this);
+            _state.Builder.Append(": ");
+            node.Types[i].Accept(this);
+
+            if (i != node.Names.Count - 1) {
+                _state.Builder.Append(", ");
+            }
+        }
+
+        if (node.Expressions.Count != 0) {
+            _state.Builder.Append(" = ");
+            RenderDelimited(node.Expressions, ", ");
+        }
+
+        _state.Builder.AppendLine();
+    }
+
+    public override void VisitDoStatement(DoStatement node) {
+        _state.AppendIndentedLine("do");
+        _state.PushIndent();
+
+        node.Block.Accept(this);
+
+        _state.PopIndent();
+        _state.AppendIndentedLine("end");
+    }
+
+    public override void VisitAssignment(Assignment node) {
+        _state.AppendIndent();
+        RenderDelimited(node.Vars, ", ");
+        _state.Builder.Append(" = ");
+        RenderDelimited(node.Expressions, ", ");
+        _state.Builder.AppendLine();
+    }
+
+    public override void VisitFunctionCall(FunctionCall node) {
+        node.Prefix.Accept(this);
+
+        RenderList(node.Suffixes);
+    }
+
+    public override void VisitNamePrefix(NamePrefix node) {
+        _state.Builder.Append(node.Name);
+    }
+
+    public override void VisitAnonymousCall(AnonymousCall node) {
+        _state.Builder.Append('(');
+
+        node.Arguments.Accept(this);
+
+        _state.Builder.Append(')');
+    }
+
+    public override void VisitFunctionArgs(FunctionArgs node) {
+        RenderDelimited(node.Arguments, ", ");
+    }
+
+    public override void VisitTableConstructor(TableConstructor node) {
+        if (node.Fields.Count == 0) {
+            _state.Builder.Append("{}");
+
+            return;
+        }
+
+        _state.Builder.AppendLine("{");
+        _state.PushIndent();
+
+        RenderPunctuatedLine(node.Fields, ", ");
+
+        _state.PopIndent();
+        _state.AppendIndented("}");
+    }
+
+    public override void VisitNameKey(NameKey node) {
+        _state.AppendIndent();
+        _state.Builder.Append(node.Key);
+        _state.Builder.Append(" = ");
+        node.Value.Accept(this);
+    }
+
+    public override void VisitAnonymousFunction(AnonymousFunction node) {
+        _state.Builder.Append("function");
+        node.Body.Accept(this);
+        _state.AppendIndented("end");
+    }
+
+    public override void VisitFunctionBody(FunctionBody node) {
+        _state.Builder.Append('(');
+        RenderDelimited(node.Parameters, ", ");
+        _state.Builder.Append("): ");
+
+        node.ReturnType.Accept(this);
+        _state.Builder.AppendLine();
+
+        _state.PushIndent();
+        node.Body.Accept(this);
+        _state.PopIndent();
+    }
+
+    public override void VisitReturn(Return node) {
+        _state.AppendIndented("return ");
+        RenderDelimited(node.Returns, ", ");
+        _state.Builder.AppendLine();
+    }
+
+    public override void VisitStringExpression(StringExpression node) {
+        _state.Builder.Append($"\"{node.Value}\"");
+    }
+
+    public override void VisitNameParameter(NameParameter node) {
+        _state.Builder.Append(node.Name);
+    }
+
+    public override void VisitEllipsisParameter(EllipsisParameter node) {
+        _state.Builder.Append("...");
+    }
+
+    public override void VisitVarName(VarName node) {
+        _state.Builder.Append(node.Name);
+    }
+
+    public override void VisitSymbolExpression(SymbolExpression node) {
+        _state.Builder.Append(node.Value);
+    }
+
+    public override void VisitIntersectionTypeInfo(IntersectionTypeInfo node) {
+        RenderDelimited(node.Types, " & ");
+    }
+
+    private void RenderDelimited<T>(List<T> items, string delimiter) where T : AstNode {
+        for (var i = 0; i < items.Count; i++) {
+            items[i].Accept(this);
+
+            if (i != items.Count - 1) {
+                _state.Builder.Append(delimiter);
+            }
+        }
+    }
+
+    private void RenderPunctuatedLine<T>(List<T> items, string delimiter) where T : AstNode {
+        foreach (var item in items) {
+            item.Accept(this);
+
+            _state.Builder.AppendLine(delimiter);
+        }
+    }
+
+    private void RenderList<T>(List<T> items) where T : AstNode {
+        foreach (var item in items) {
+            item.Accept(this);
+        }
+    }
+}
