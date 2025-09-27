4f5a6cf490f8d153705db6a1c3793d1d13b8ff9a
glomdom <sadonkovkiril@gmail.com>
2025-09-27 13:36:45 +0300

    fix: propagate errors instead of swallowing

--- RobloxCS.CLI/Program.cs
+++ RobloxCS.CLI/Program.cs
@@ -10,6 +10,7 @@ internal static class Program {
             config.SetApplicationName("rbxcsc");
             config.SetApplicationVersion("0.0.0a");
             config.UseStrictParsing();
+            config.PropagateExceptions();
 
             config.AddBranch<CompileSettings>("compile", branch => {
                 branch.SetDescription("Compile a C# file to Luau or a C# project");
