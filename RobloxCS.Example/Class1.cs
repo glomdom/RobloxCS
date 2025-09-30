5c5def4724579c6626056fe76725739583b828cd
glomdom <sadonkovkiril@gmail.com>
2025-09-30 15:47:22 +0300

    chore: truncate test

--- RobloxCS.Example/Class1.cs
+++ RobloxCS.Example/Class1.cs
@@ -2,41 +2,6 @@
 
 internal class Class1 {
     public int A = 0;
-    public readonly string Name;
-
-    public Class1(string name) {
-        Name = name;
-        A = 10;
-
-        var xx = A + 5;
-
-        {
-            var xxShadow = xx * 2;
-            xx = xxShadow;
-        }
-
-        if (xx > 10) {
-            A = xx;
-        } else if (xx < 10) {
-            A = -1;
-        } else if (xx == 12) {
-            A = -2;
-        } else {
-            A = -3;
-        }
-
-        while (A < 20) {
-            A++;
-        }
-
-        for (var i = 0; i < 3; i++) {
-            A += i;
-        }
-
-        for (int i = 0, j = 5; i < j; i++, j--) {
-            A += i + j;
-        }
-    }
 
     public int DoubleA() {
         var result = A * 2;
