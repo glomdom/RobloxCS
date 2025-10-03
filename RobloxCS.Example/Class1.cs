4dc27966eea52bc63c94f861d2ef852495c3383f
glomdom <sadonkovkiril@gmail.com>
2025-10-03 14:13:34 +0300

    chore: example upgrade

--- RobloxCS.Example/Class1.cs
+++ RobloxCS.Example/Class1.cs
@@ -3,6 +3,12 @@
 internal class Class1 {
     public int A = 0;
 
+    public int TernaryVariableTest() {
+        var v = A > 4 ? 1 : 2;
+
+        return v;
+    }
+    
     public int TernaryReturnTest() {
         return A > 4 ? 1 : 2;
     }
