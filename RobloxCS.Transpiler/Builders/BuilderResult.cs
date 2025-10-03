b809a676e0208963e4a1efda23c8d08cae43e394
glomdom <sadonkovkiril@gmail.com>
2025-10-03 13:10:34 +0300

    feat: builder result

---
+++ RobloxCS.Transpiler/Builders/BuilderResult.cs
@@ -0,0 +1,13 @@
+﻿using RobloxCS.AST.Statements;
+
+namespace RobloxCS.Transpiler.Builders;
+
+public record BuilderResult {
+    public List<Statement> Statements { get; }
+
+    private BuilderResult(IEnumerable<Statement> statements) {
+        Statements = statements.ToList();
+    }
+
+    public static BuilderResult FromSingle(Statement stmt) => new([stmt]);
+}
