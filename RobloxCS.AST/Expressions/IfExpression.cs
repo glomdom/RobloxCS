0aae5f082ed8b26f8f0ab8d41ff66f53baf93220
glomdom <sadonkovkiril@gmail.com>
2025-10-04 21:41:29 +0300

    fix: add true expression

--- RobloxCS.AST/Expressions/IfExpression.cs
+++ RobloxCS.AST/Expressions/IfExpression.cs
@@ -2,10 +2,11 @@
 
 public sealed class IfExpression : Expression {
     public required Expression Condition { get; set; }
+    public required Expression If { get; set; }
     public List<ElseIfExpression>? ElseIfExpressions { get; set; }
-    public required Expression ElseExpression { get; set; }
+    public required Expression Else { get; set; }
 
-    public static IfExpression From(Expression cond, Expression elseExpr) => new() { Condition = cond, ElseExpression = elseExpr };
+    public static IfExpression From(Expression cond, Expression ifExpr, Expression elseExpr) => new() { Condition = cond, If = ifExpr, Else = elseExpr };
 
     public override AstNode DeepClone() => throw new NotImplementedException();
     public override void Accept(IAstVisitor v) => v.VisitIfExpression(this);
