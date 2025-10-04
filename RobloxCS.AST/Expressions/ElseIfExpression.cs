55e0800c7757da5043ff8ca9eb68202c5eb0014f
glomdom <sadonkovkiril@gmail.com>
2025-10-04 21:37:59 +0300

    feat: dump some sugar on elseif

--- RobloxCS.AST/Expressions/ElseIfExpression.cs
+++ RobloxCS.AST/Expressions/ElseIfExpression.cs
@@ -3,7 +3,9 @@
 public sealed class ElseIfExpression : Expression {
     public required Expression Condition { get; set; }
     public required Expression Expression { get; set; }
-    
+
+    public static ElseIfExpression From(Expression cond, Expression expr) => new() { Condition = cond, Expression = expr };
+
     public override AstNode DeepClone() => throw new NotImplementedException();
     public override void Accept(IAstVisitor v) => v.VisitElseIfExpression(this);
     public override T Accept<T>(IAstVisitor<T> v) => v.VisitElseIfExpression(this);
--- RobloxCS.AST/Expressions/IfExpression.cs
+++ RobloxCS.AST/Expressions/IfExpression.cs
@@ -2,9 +2,11 @@
 
 public sealed class IfExpression : Expression {
     public required Expression Condition { get; set; }
-    public required List<ElseIfExpression> ElseIfExpressions { get; set; }
+    public List<ElseIfExpression>? ElseIfExpressions { get; set; }
     public required Expression ElseExpression { get; set; }
-    
+
+    public static IfExpression From(Expression cond, Expression elseExpr) => new() { Condition = cond, ElseExpression = elseExpr };
+
     public override AstNode DeepClone() => throw new NotImplementedException();
     public override void Accept(IAstVisitor v) => v.VisitIfExpression(this);
     public override T Accept<T>(IAstVisitor<T> v) => v.VisitIfExpression(this);
