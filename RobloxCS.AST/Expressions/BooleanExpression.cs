2f8a7abe73034ae273b14f8a3444863e0125632c
glomdom <sadonkovkiril@gmail.com>
2025-10-06 21:48:14 +0300

    chore: bool expression sugar

--- RobloxCS.AST/Expressions/BooleanExpression.cs
+++ RobloxCS.AST/Expressions/BooleanExpression.cs
@@ -4,6 +4,7 @@ public sealed class BooleanExpression : Expression {
     public required bool Value { get; set; }
 
     public static BooleanExpression True() => new() { Value = true };
+    public static BooleanExpression False() => new() { Value = false };
 
     public override BooleanExpression DeepClone() => new() { Value = Value };
     public override void Accept(IAstVisitor v) => v.VisitBooleanExpression(this);
