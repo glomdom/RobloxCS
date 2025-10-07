e827113da8faf84e1967e621f794cc6e759e1514
glomdom <sadonkovkiril@gmail.com>
2025-10-07 21:55:10 +0300

    chore: expression bodify

--- RobloxCS.AST/Block.cs
+++ RobloxCS.AST/Block.cs
@@ -5,39 +5,21 @@ namespace RobloxCS.AST;
 public sealed class Block : AstNode {
     public required List<Statement> Statements { get; set; }
 
-    public static Block Empty() {
-        return new Block { Statements = [] };
-    }
+    public static Block Empty() => new() { Statements = [] };
+    public static Block From(Statement stmt) => new() { Statements = [stmt] };
+    public static Block From(List<Statement> stmts) => new() { Statements = stmts };
 
-    public static Block From(Statement stmt) {
-        return new Block { Statements = [stmt] };
-    }
-    
-    public static Block From(List<Statement> stmts) {
-        return new Block { Statements = stmts };
-    }
-
-    public void AddStatement(Statement statement) {
-        Statements.Add(statement);
-    }
-
-    public void AddStatements(List<Statement> stmts) {
-        Statements.AddRange(stmts);
-    }
+    public void AddStatement(Statement statement) => Statements.Add(statement);
+    public void AddStatements(List<Statement> stmts) => Statements.AddRange(stmts);
 
     /// <summary>
     /// Flattens the provided block into the block.
     /// </summary>
-    public void AddBlock(Block block) {
-        Statements.AddRange(block.Statements);
-    }
+    public void AddBlock(Block block) => Statements.AddRange(block.Statements);
 
     public override Block DeepClone() => new() { Statements = Statements.Select(s => (Statement)s.DeepClone()).ToList() };
     public override void Accept(IAstVisitor v) => v.VisitBlock(this);
     public override T Accept<T>(IAstVisitor<T> v) => v.VisitBlock(this);
 
-    public override IEnumerable<Statement> Children() {
-        return Statements;
-    }
-
+    public override IEnumerable<Statement> Children() => Statements;
 }
