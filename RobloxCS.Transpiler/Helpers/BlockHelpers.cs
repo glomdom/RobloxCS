35f7f869a0412be44f9718da9a8e84dfff49f565
glomdom <sadonkovkiril@gmail.com>
2025-11-22 14:43:43 +0200

    feat: block helper changes parents

--- RobloxCS.Transpiler/Helpers/BlockHelpers.cs
+++ RobloxCS.Transpiler/Helpers/BlockHelpers.cs
@@ -9,12 +9,30 @@ public static class BlockHelpers {
     public static Block From(List<Statement> stmts) => new() { Statements = stmts };
 
     extension(Block block) {
-        public void AddStatement(Statement statement) => block.Statements.Add(statement);
-        public void AddStatements(List<Statement> stmts) => block.Statements.AddRange(stmts);
+        /// <summary>
+        /// Adds a statement to the block, changing the statements parent.
+        /// </summary>
+        /// <param name="statement">The statement that will be added to the block.</param>
+        public void AddStatement(Statement statement) {
+            block.Statements.Add(statement);
+            statement.Parent = block;
+        }
+
+        /// <summary>
+        /// Adds each statement to the block, changing their parent.
+        /// </summary>
+        /// <param name="stmts">The statements which will be added to the block.</param>
+        public void AddStatements(List<Statement> stmts) {
+            foreach (var stmt in stmts) {
+                block.AddStatement(stmt);
+            }
+        }
 
         /// <summary>
         /// Flattens the provided block into the block.
         /// </summary>
-        public void AddBlock(Block other) => block.Statements.AddRange(other.Statements);
+        public void AddBlock(Block other) {
+            block.Statements.AddRange(other.Statements);
+        }
     }
 }
