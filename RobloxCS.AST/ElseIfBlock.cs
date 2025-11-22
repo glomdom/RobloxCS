9f86b9e30741851dc68ad0e456b7aba21f1c85d4
glomdom <sadonkovkiril@gmail.com>
2025-11-22 14:29:58 +0200

    feat: parent link elseif

--- RobloxCS.AST/ElseIfBlock.cs
+++ RobloxCS.AST/ElseIfBlock.cs
@@ -1,10 +1,13 @@
 ﻿using RobloxCS.AST.Expressions;
+using RobloxCS.AST.Statements;
 
 namespace RobloxCS.AST;
 
 public sealed class ElseIfBlock : AstNode {
     public required Expression Condition { get; set; }
     public required Block Block { get; set; }
+
+    public IfStatement? ParentIf => Parent as IfStatement;
 
     public override AstNode DeepClone() => throw new NotImplementedException();
 
