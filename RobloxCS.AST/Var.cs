060e30cf9e61a3d0ab97017fcb5c32c854c4cd70
glomdom <sadonkovkiril@gmail.com>
2025-11-22 14:28:11 +0200

    chore: parent link var

--- RobloxCS.AST/Var.cs
+++ RobloxCS.AST/Var.cs
@@ -7,6 +7,8 @@ public abstract class Var : AstNode;
 public sealed class VarExpression : Var {
     public required Expression Expression { get; set; }
 
+    public Var? ParentVar => Parent as Var;
+
     public static VarExpression FromExpression(Expression expr) => new() { Expression = expr };
 
     public override VarExpression DeepClone() => new() { Expression = (Expression)Expression.DeepClone() };
@@ -20,6 +22,8 @@ public sealed class VarExpression : Var {
 
 public sealed class VarName : Var {
     public required string Name { get; set; }
+
+    public Var? ParentVar => Parent as Var;
 
     public static VarName FromSymbol(SymbolExpression sym) => new() { Name = sym.Value };
     public static VarName FromString(string str) => new() { Name = str };
