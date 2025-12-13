f172babbf3891d51eceea9bcea7a9cadc3db263d
glomdom <sadonkovkiril@gmail.com>
2025-12-13 23:38:10 +0200

    feat: add transient block

---
+++ RobloxCS.AST/Transient/TransientBlock.cs
@@ -0,0 +1,12 @@
+﻿using RobloxCS.AST.Statements;
+
+namespace RobloxCS.AST.Transient;
+
+/// <summary>
+/// A block containing statements. Can be standalone or inside a container.
+/// </summary>
+public sealed class TransientBlock : TransientStatement {
+    public required List<Statement> Statements { get; set; } = [];
+    
+    public override AstNode DeepClone() => throw new NotImplementedException();
+}
