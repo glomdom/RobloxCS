0f7a8692ab8edc94f8b0633d9b6d77648e6a921d
glomdom <sadonkovkiril@gmail.com>
2025-12-14 18:10:05 +0200

    chore: better test

---
+++ RobloxCS.Tests/Data/NestedContinueControlFlow.cs
@@ -0,0 +1,35 @@
+﻿namespace RobloxCS.Tests.ControlFlow;
+
+internal class NestedContinueControlFlow {
+    internal void Main() {
+        for (var x = 0; x < 3; x++) {
+            for (var y = 0; y < 3; y++) {
+                if (x == 1 && y == 1) {
+                    Console.WriteLine("Skipping Center");
+
+                    continue;
+                }
+
+                if (x == 2 && y == 2) {
+                    Console.WriteLine("Breaking Inner");
+
+                    break;
+                }
+
+                Console.WriteLine(x);
+                Console.WriteLine(y);
+            }
+        }
+
+        var count = 3;
+        do {
+            Console.WriteLine(count);
+
+            count--;
+
+            if (count == 1) {
+                Console.WriteLine("Hit 1");
+            }
+        } while (count > 0);
+    }
+}
