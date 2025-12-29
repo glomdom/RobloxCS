8eb89fc1d1537cf54a188154f67a283acc87ffec
glomdom <sadonkovkiril@gmail.com>
2025-12-30 01:34:52 +0200

    feat(api/types): stub out type gen

---
+++ RobloxCS.Types/RobloxNativeAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace RobloxCS.Types;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+internal class RobloxNativeAttribute : Attribute {
+    public string RobloxName { get; }
+    public RobloxNativeType NativeType { get; }
+
+    public RobloxNativeAttribute(string name, RobloxNativeType type) {
+        RobloxName = name;
+        NativeType = type;
+    }
+}
---
+++ RobloxCS.Types/RobloxNativeType.cs
@@ -0,0 +1,21 @@
+﻿namespace RobloxCS.Types;
+
+internal enum RobloxNativeType {
+    /// <summary>
+    /// Corresponds to an <c>Instance</c> in Roblox.
+    /// Generated code will take the form of <c>Instance.new(robloxName)</c>.
+    /// </summary>
+    Instance,
+
+    /// <summary>
+    /// Corresponds to a <c>Service</c> in Roblox.
+    /// Generated code will take the form of <c>game:GetService(robloxName)</c>.
+    /// </summary>
+    Service,
+
+    /// <summary>
+    /// Corresponds to a datatype in Roblox.
+    /// Generated code will take the form of <c>robloxName.new()</c>.
+    /// </summary>
+    DataType,
+}
