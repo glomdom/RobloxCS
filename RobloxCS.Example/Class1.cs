1ab2fa7644f44158a5954651467ac06cb2880ffe
glomdom <sadonkovkiril@gmail.com>
2025-12-30 01:22:23 +0200

    chore: wipe testing example

--- RobloxCS.Example/Class1.cs
+++ RobloxCS.Example/Class1.cs
@@ -1,28 +1,5 @@
 ﻿namespace RobloxCS.Example;
 
-internal class Hero {
-    public string Name;
-    public int Health;
-
-    public Hero(string name, int hp) {
-        Name = name;
-        Health = hp;
-    }
-
-    public void Heal(int amount) {
-        Health += amount;
-    }
-}
-
 internal class Program {
-    internal void Main() {
-        var h = new Hero("Noob", 100);
-
-        Console.WriteLine("Hero: " + h.Name);
-
-        h.Heal(50);
-
-        Console.WriteLine("Health: " + h.Health);
-        Console.Write("how do i map this to luau");
-    }
+    internal void Main() { }
 }
