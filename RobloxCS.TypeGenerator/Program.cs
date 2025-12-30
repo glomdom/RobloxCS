ee99b375d874eb720f79ca277c2d26094c3ae2cc
glomdom <sadonkovkiril@gmail.com>
2025-12-30 02:09:18 +0200

    chore: follow conventions

--- RobloxCS.TypeGenerator/Program.cs
+++ RobloxCS.TypeGenerator/Program.cs
@@ -5,19 +5,19 @@ using Serilog.Events;
 namespace RobloxCS.TypeGenerator;
 
 internal static class Program {
-    private const string BASE_URL = "https://setup.rbxcdn.com";
-    private const string VERSION_HASH_URL = $"{BASE_URL}/versionQTStudio";
+    private const string BaseUrl = "https://setup.rbxcdn.com";
+    private const string VersionHashUrl = $"{BaseUrl}/versionQTStudio";
     
     internal static async Task Main(string[] args) {
         LoggerSetup.LevelSwitch.MinimumLevel = LogEventLevel.Verbose;
         LoggerSetup.Configure();
         
         using var http = new HttpClient();
-        var versionHash = await http.GetStringAsync(VERSION_HASH_URL);
+        var versionHash = await http.GetStringAsync(VersionHashUrl);
             
         Log.Information("Generating API dump for {VersionHash}", versionHash);
 
-        var apiDumpUrl = $"{BASE_URL}/{versionHash}-Full-API-Dump.json";
+        var apiDumpUrl = $"{BaseUrl}/{versionHash}-Full-API-Dump.json";
         var apiDumpJson = await http.GetStringAsync(apiDumpUrl);
         
         Log.Information("Received api dump {JSON}", apiDumpJson);
