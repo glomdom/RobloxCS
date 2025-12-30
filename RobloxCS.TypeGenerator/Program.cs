c9f90e34d49e5146a7b69aa9f5deadfa1abd0840
glomdom <sadonkovkiril@gmail.com>
2025-12-30 02:07:17 +0200

    feat: stub out type generation from fetched version

--- RobloxCS.TypeGenerator/Program.cs
+++ RobloxCS.TypeGenerator/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using RobloxCS.Common;
+using Serilog;
+using Serilog.Events;
 
-Console.WriteLine("Hello, World!");
+namespace RobloxCS.TypeGenerator;
+
+internal static class Program {
+    private const string BASE_URL = "https://setup.rbxcdn.com";
+    private const string VERSION_HASH_URL = $"{BASE_URL}/versionQTStudio";
+    
+    internal static async Task Main(string[] args) {
+        LoggerSetup.LevelSwitch.MinimumLevel = LogEventLevel.Verbose;
+        LoggerSetup.Configure();
+        
+        using var http = new HttpClient();
+        var versionHash = await http.GetStringAsync(VERSION_HASH_URL);
+            
+        Log.Information("Generating API dump for {VersionHash}", versionHash);
+    }
+}
