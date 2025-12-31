580ddd42e28fa1b5cbaa72857a9eb6a9c55e6a0d
glomdom <sadonkovkiril@gmail.com>
2025-12-31 17:31:13 +0200

    feat: serialize callbacks

--- RobloxCS.TypeGenerator/Models/RobloxCallback.cs
+++ RobloxCS.TypeGenerator/Models/RobloxCallback.cs
@@ -1,5 +1,23 @@
-﻿namespace RobloxCS.TypeGenerator.Models;
+﻿using System.Text.Json.Serialization;
+using RobloxCS.TypeGenerator.Converters;
+
+namespace RobloxCS.TypeGenerator.Models;
 
 public sealed class RobloxCallback : RobloxMember {
     public override RobloxMemberType MemberType => RobloxMemberType.Callback;
+
+    public required List<RobloxParameter> Parameters { get; init; }
+    public required RobloxType ReturnType { get; init; }
+
+    [JsonConverter(typeof(RobloxSecurityConverter))]
+    public required RobloxSecurity Security { get; init; }
+
+    public required RobloxThreadSafety ThreadSafety { get; init; }
+    public List<RobloxTagKind>? Tags { get; init; }
+
+    public override string ToString() {
+        var tagsString = Tags is not null ? $" [{string.Join(", ", Tags)}]" : string.Empty;
+
+        return $"{Name}({string.Join(", ", Parameters)}) -> {ReturnType.Name} [threadSafety={ThreadSafety}] [read={Security.Read}, write={Security.Write}]{tagsString}";
+    }
 }
