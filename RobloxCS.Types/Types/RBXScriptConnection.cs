c6e93da34c45d3ac18bd227ea88a1a929c1676df
glomdom <sadonkovkiril@gmail.com>
2026-01-09 15:07:50 +0200

    fix: add missing members to `RBXScriptConnection`

--- RobloxCS.Types/Types/RBXScriptConnection.cs
+++ RobloxCS.Types/Types/RBXScriptConnection.cs
@@ -1,5 +1,8 @@
 ﻿namespace RobloxCS.Types;
 
+[RobloxNative("RBXScriptConnection", RobloxNativeType.DataType)]
 public class RBXScriptConnection {
-    
+    public bool Connected { get; init; }
+
+    public void Disconnect() => throw new InvalidOperationException("Cannot call reserved method for RobloxCS transpiler.");
 }
