76283e04fd785e2ca8e11bfcbb58a4483db50515
glomdom <sadonkovkiril@gmail.com>
2026-01-10 21:04:41 +0200

    fix: parent in astrewriter virtuals

--- RobloxCS.AST/AstRewriter.cs
+++ RobloxCS.AST/AstRewriter.cs
@@ -1,4 +1,5 @@
-﻿using RobloxCS.AST.Expressions;
+﻿using System.Buffers;
+using RobloxCS.AST.Expressions;
 using RobloxCS.AST.Functions;
 using RobloxCS.AST.Generics;
 using RobloxCS.AST.Parameters;
@@ -13,23 +14,23 @@ namespace RobloxCS.AST;
 
 public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     AstNode IInternalAstVisitor<AstNode>.VisitTransientServiceUsageExpression(TransientServiceUsageExpression node) {
-        node.AccessExpression = Visit(node.AccessExpression);
+        node.AccessExpression = Visit(node.AccessExpression, node);
 
         return node;
     }
 
     AstNode IInternalAstVisitor<AstNode>.VisitTransientBlock(TransientBlock node) {
-        VisitList(node.Statements);
+        VisitList(node.Statements, node);
 
         return node;
     }
 
     AstNode IInternalAstVisitor<AstNode>.VisitTransientForLoop(TransientForLoop node) {
-        VisitList(node.Initializers);
-        if (node.Condition is not null) node.Condition = Visit(node.Condition);
+        VisitList(node.Initializers, node);
+        if (node.Condition is not null) node.Condition = Visit(node.Condition, node);
 
-        VisitList(node.Incrementors);
-        node.Body = Visit(node.Body);
+        VisitList(node.Incrementors, node);
+        node.Body = Visit(node.Body, node);
 
         return node;
     }
@@ -46,31 +47,31 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitParameter(Parameter node) => node;
 
     public virtual AstNode VisitBlock(Block node) {
-        VisitList(node.Statements);
+        VisitList(node.Statements, node);
 
         return node;
     }
 
     public virtual AstNode VisitChunk(Chunk node) {
-        node.Block = Visit(node.Block);
+        node.Block = Visit(node.Block, node);
 
         return node;
     }
 
     public virtual AstNode VisitFunctionArgs(FunctionArgs node) {
-        VisitList(node.Arguments);
+        VisitList(node.Arguments, node);
 
         return node;
     }
 
     public virtual AstNode VisitFunctionBody(FunctionBody node) {
-        if (node.Generics is not null) VisitList(node.Generics);
+        if (node.Generics is not null) VisitList(node.Generics, node);
 
-        VisitList(node.Parameters);
-        VisitList(node.TypeSpecifiers);
+        VisitList(node.Parameters, node);
+        VisitList(node.TypeSpecifiers, node);
 
-        node.ReturnType = Visit(node.ReturnType);
-        node.Body = Visit(node.Body);
+        node.ReturnType = Visit(node.ReturnType, node);
+        node.Body = Visit(node.Body, node);
 
         return node;
     }
@@ -78,14 +79,14 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitFunctionName(FunctionName node) => node;
 
     public virtual AstNode VisitAnonymousFunction(AnonymousFunctionExpression node) {
-        node.Body = Visit(node.Body);
+        node.Body = Visit(node.Body, node);
 
         return node;
     }
 
     public virtual AstNode VisitBinaryOperatorExpression(BinaryOperatorExpression node) {
-        node.Left = Visit(node.Left);
-        node.Right = Visit(node.Right);
+        node.Left = Visit(node.Left, node);
+        node.Right = Visit(node.Right, node);
 
         return node;
     }
@@ -94,8 +95,8 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitElseIfExpression(ElseIfExpression node) => throw new NotImplementedException();
 
     public virtual AstNode VisitFunctionCall(FunctionCallExpression node) {
-        node.Prefix = Visit(node.Prefix);
-        VisitList(node.Suffixes);
+        node.Prefix = Visit(node.Prefix, node);
+        VisitList(node.Suffixes, node);
 
         return node;
     }
@@ -104,7 +105,7 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitNumberExpression(NumberExpression node) => node;
 
     public virtual AstNode VisitParenthesisExpression(ParenthesisExpression node) {
-        node.Expression = Visit(node.Expression);
+        node.Expression = Visit(node.Expression, node);
 
         return node;
     }
@@ -113,7 +114,7 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitSymbolExpression(SymbolExpression node) => node;
 
     public virtual AstNode VisitTableConstructor(TableConstructorExpression node) {
-        VisitList(node.Fields);
+        VisitList(node.Fields, node);
 
         return node;
     }
@@ -121,7 +122,7 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitNoKey(NoKey node) => throw new NotImplementedException();
 
     public virtual AstNode VisitNameKey(NameKey node) {
-        node.Value = Visit(node.Value);
+        node.Value = Visit(node.Value, node);
 
         return node;
     }
@@ -129,7 +130,7 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitTypeAssertionExpression(TypeAssertionExpression node) => throw new NotImplementedException();
 
     public virtual AstNode VisitUnaryOperatorExpression(UnaryOperatorExpression node) {
-        node.Expression = Visit(node.Expression);
+        node.Expression = Visit(node.Expression, node);
 
         return node;
     }
@@ -144,7 +145,7 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitNameParameter(NameParameter node) => node;
 
     public virtual AstNode VisitExpressionPrefix(ExpressionPrefix node) {
-        node.Expression = Visit(node.Expression);
+        node.Expression = Visit(node.Expression, node);
 
         return node;
     }
@@ -152,8 +153,8 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitNamePrefix(NamePrefix node) => node;
 
     public virtual AstNode VisitAssignment(AssignmentStatement node) {
-        VisitList(node.Vars);
-        VisitList(node.Expressions);
+        VisitList(node.Vars, node);
+        VisitList(node.Expressions, node);
 
         return node;
     }
@@ -161,8 +162,8 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitBreakStatement(BreakStatement node) => node;
 
     public virtual AstNode VisitCompoundAssignmentStatement(CompoundAssignmentStatement node) {
-        node.Left = Visit(node.Left);
-        node.Right = Visit(node.Right);
+        node.Left = Visit(node.Left, node);
+        node.Right = Visit(node.Right, node);
 
         return node;
     }
@@ -170,83 +171,83 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitContinueStatement(ContinueStatement node) => node;
 
     public virtual AstNode VisitDoStatement(DoStatement node) {
-        node.Block = Visit(node.Block);
+        node.Block = Visit(node.Block, node);
 
         return node;
     }
 
     public virtual AstNode VisitFunctionDeclaration(FunctionDeclarationStatement node) {
-        node.Name = Visit(node.Name);
-        node.Body = Visit(node.Body);
+        node.Name = Visit(node.Name, node);
+        node.Body = Visit(node.Body, node);
 
         return node;
     }
 
     public virtual AstNode VisitIfStatement(IfStatement node) {
-        node.Condition = Visit(node.Condition);
-        node.Block = Visit(node.Block);
+        node.Condition = Visit(node.Condition, node);
+        node.Block = Visit(node.Block, node);
 
-        if (node.ElseIf is not null) VisitList(node.ElseIf);
-        if (node.Else is not null) node.Else = Visit(node.Else);
+        if (node.ElseIf is not null) VisitList(node.ElseIf, node);
+        if (node.Else is not null) node.Else = Visit(node.Else, node);
 
         return node;
     }
 
     public virtual AstNode VisitInterpolatedStringExpression(InterpolatedStringExpression node) {
-        VisitList(node.Segments);
+        VisitList(node.Segments, node);
 
         return node;
     }
 
     public virtual AstNode VisitInterpolatedStringSegment(InterpolatedStringSegment node) {
-        node.Expression = Visit(node.Expression);
+        node.Expression = Visit(node.Expression, node);
 
         return node;
     }
 
     public virtual AstNode VisitLocalAssignment(LocalAssignmentStatement node) {
-        VisitList(node.Names);
-        VisitList(node.Expressions);
-        VisitList(node.Types);
+        VisitList(node.Names, node);
+        VisitList(node.Expressions, node);
+        VisitList(node.Types, node);
 
         return node;
     }
 
     public virtual AstNode VisitRepeatStatement(RepeatStatement node) {
-        node.Block = Visit(node.Block);
-        node.Until = Visit(node.Until);
+        node.Block = Visit(node.Block, node);
+        node.Until = Visit(node.Until, node);
 
         return node;
     }
 
     public virtual AstNode VisitFunctionCallStatement(FunctionCallStatement node) {
-        node.Prefix = Visit(node.Prefix);
-        VisitList(node.Suffixes);
+        node.Prefix = Visit(node.Prefix, node);
+        VisitList(node.Suffixes, node);
 
         return node;
     }
 
     public virtual AstNode VisitReturn(ReturnStatement node) {
-        VisitList(node.Returns);
+        VisitList(node.Returns, node);
 
         return node;
     }
 
     public virtual AstNode VisitWhileStatement(WhileStatement node) {
-        node.Condition = Visit(node.Condition);
-        node.Block = Visit(node.Block);
+        node.Condition = Visit(node.Condition, node);
+        node.Block = Visit(node.Block, node);
 
         return node;
     }
 
     public virtual AstNode VisitAnonymousCall(AnonymousCall node) {
-        node.Arguments = Visit(node.Arguments);
+        node.Arguments = Visit(node.Arguments, node);
 
         return node;
     }
 
     public virtual AstNode VisitMethodCall(MethodCall node) {
-        node.Args = Visit(node.Args);
+        node.Args = Visit(node.Args, node);
 
         return node;
     }
@@ -254,7 +255,7 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitCall(Call node) => throw new NotImplementedException();
 
     public virtual AstNode VisitDot(Dot node) {
-        node.Name = Visit(node.Name);
+        node.Name = Visit(node.Name, node);
 
         return node;
     }
@@ -262,7 +263,7 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitIndex(Index node) => node;
 
     public virtual AstNode VisitArrayTypeInfo(ArrayTypeInfo node) {
-        node.ElementType = Visit(node.ElementType);
+        node.ElementType = Visit(node.ElementType, node);
 
         return node;
     }
@@ -271,10 +272,10 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitBooleanTypeInfo(BooleanTypeInfo node) => throw new NotImplementedException();
 
     public virtual AstNode VisitCallbackTypeInfo(CallbackTypeInfo node) {
-        if (node.Generics is not null) node.Generics = Visit(node.Generics);
+        if (node.Generics is not null) node.Generics = Visit(node.Generics, node);
 
-        VisitList(node.Arguments);
-        node.ReturnType = Visit(node.ReturnType);
+        VisitList(node.Arguments, node);
+        node.ReturnType = Visit(node.ReturnType, node);
 
         return node;
     }
@@ -283,27 +284,27 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitStringTypeInfo(StringTypeInfo node) => throw new NotImplementedException();
 
     public virtual AstNode VisitTableTypeInfo(TableTypeInfo node) {
-        VisitList(node.Fields);
+        VisitList(node.Fields, node);
 
         return node;
     }
 
     public virtual AstNode VisitTypeArgument(TypeArgument node) {
-        node.TypeInfo = Visit(node.TypeInfo);
+        node.TypeInfo = Visit(node.TypeInfo, node);
 
         return node;
     }
 
     public virtual AstNode VisitTypeDeclaration(TypeDeclarationStatement node) {
-        if (node.Declarations is not null) VisitList(node.Declarations);
-        node.DeclareAs = Visit(node.DeclareAs);
+        if (node.Declarations is not null) VisitList(node.Declarations, node);
+        node.DeclareAs = Visit(node.DeclareAs, node);
 
         return node;
     }
 
     public virtual AstNode VisitTypeField(TypeField node) {
-        node.Key = Visit(node.Key);
-        node.Value = Visit(node.Value);
+        node.Key = Visit(node.Key, node);
+        node.Value = Visit(node.Value, node);
 
         return node;
     }
@@ -315,8 +316,8 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     public virtual AstNode VisitUnionTypeInfo(UnionTypeInfo node) => throw new NotImplementedException();
 
     public virtual AstNode VisitVarExpression(VarExpression node) {
-        node.Prefix = Visit(node.Prefix);
-        VisitList(node.Suffixes);
+        node.Prefix = Visit(node.Prefix, node);
+        VisitList(node.Suffixes, node);
 
         return node;
     }
@@ -326,7 +327,7 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
     /// <summary>
     /// Visits every node in the provided list and rewrites them if required.
     /// </summary>
-    protected void VisitList<T>(IList<T> list) where T : AstNode {
+    protected void VisitList<T>(IList<T> list, AstNode owner) where T : AstNode {
         for (var i = 0; i < list.Count; i++) {
             var oldNode = list[i];
             var newNode = oldNode.Accept(this);
@@ -338,6 +339,8 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
                 continue;
             }
 
+            newNode.Parent = owner;
+
             if (ReferenceEquals(oldNode, newNode)) continue;
 
             if (newNode is T typedNode) {
@@ -348,5 +351,15 @@ public class AstRewriter : IAstVisitor<AstNode>, IInternalAstVisitor<AstNode> {
         }
     }
 
-    private T Visit<T>(T nodeProp) where T : AstNode => (T)nodeProp.Accept(this);
+    protected T Visit<T>(T nodeProp, AstNode parent) where T : AstNode {
+        if (ReferenceEquals(nodeProp, null)) return null!;
+
+        var newNode = nodeProp.Accept(this);
+
+        if (!ReferenceEquals(newNode, null)) {
+            newNode.Parent = parent;
+        }
+
+        return (T)newNode!;
+    }
 }
