1b354675a0a1ca9d429ffa614cf3035319de3b1f
glomdom <sadonkovkiril@gmail.com>
2026-01-10 22:18:46 +0200

    feat: naive object pool example

---
+++ RobloxCS.Example/ObjectPool.cs
@@ -0,0 +1,28 @@
+﻿using RobloxCS.Types;
+
+namespace RobloxCS.Example;
+
+public class ObjectPool {
+    public List<BasePart> Pool = [];
+
+    public BasePart Get() {
+        BasePart inst;
+
+        if (Pool.Count > 0) {
+            var last = Pool.Count - 1;
+            inst = Pool[last];
+
+            Pool.RemoveAt(last);
+        } else {
+            inst = new BasePart();
+        }
+
+        return inst;
+    }
+
+    public void Release(BasePart inst) {
+        inst.Parent = null!;
+
+        Pool.Add(inst);
+    }
+}
