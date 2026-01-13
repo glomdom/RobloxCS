f356217e1b8662b4ef140e76684c4047e03b4416
glomdom <sadonkovkiril@gmail.com>
2026-01-13 16:38:59 +0200

    feat: get msbuild stuff from csproj

--- RobloxCS.CLI/Commands/Compile/ProjectCompileCommand.cs
+++ RobloxCS.CLI/Commands/Compile/ProjectCompileCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel;
 using JetBrains.Annotations;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis.MSBuild;
 using Microsoft.Extensions.FileSystemGlobbing;
 using RobloxCS.Common;
 using RobloxCS.Compiler;
@@ -14,7 +16,7 @@ namespace RobloxCS.CLI.Commands.Compile;
 
 [Description("Compile a C# project.")]
 [UsedImplicitly]
-public sealed class ProjectCompileCommand : Command<ProjectCompileCommand.Settings> {
+public sealed class ProjectCompileCommand : AsyncCommand<ProjectCompileCommand.Settings> {
     private readonly IAnsiConsole _console;
 
     public ProjectCompileCommand(IAnsiConsole console) {
@@ -27,15 +29,36 @@ public sealed class ProjectCompileCommand : Command<ProjectCompileCommand.Settin
         // public required string Path { get; set; }
     }
 
-    protected override int Execute(CommandContext context, Settings settings, CancellationToken cancellation) {
+    protected override async Task<int> ExecuteAsync(CommandContext context, Settings settings, CancellationToken cancellation) {
         LoggerSetup.LevelSwitch.MinimumLevel = settings.Verbosity ? LogEventLevel.Verbose : LogEventLevel.Warning;
 
         var fullCwd = Path.GetFullPath(Environment.CurrentDirectory);
-        Log.Information("Searching for candidate .csproj files in {}", fullCwd);
+        Log.Information("Searching for candidate .csproj files in {SearchDirectory}", fullCwd);
 
         var candidateMatcher = new Matcher().AddInclude("*.csproj");
         var csprojCandidates = candidateMatcher.GetResultsInFullPath(fullCwd).ToList();
-        //
+
+        if (csprojCandidates.Count == 0) {
+            Log.Error("Failed to find a .csproj file in the current directory.");
+
+            return -1;
+        }
+
+        var csprojFile = csprojCandidates.First();
+        Log.Information("Compiling project from {ProjectFilePath}", csprojFile);
+
+        var vsi = MSBuildLocator.RegisterDefaults();
+        Log.Information("Registered MSBuild defaults");
+
+        using var workspace = MSBuildWorkspace.Create();
+        Log.Information("Created MSBuild workspace");
+
+        var project = await workspace.OpenProjectAsync(csprojFile, cancellationToken: cancellation);
+        Log.Information("Read MSBuild properties");
+
+        var compilation = await project.GetCompilationAsync(cancellation);
+        Log.Information("Got C# compilation");
+
         // Log.Information("Creating C# compiler");
         // var compiler = new CSharpCompiler(settings.Path);
         // var diagnosticMessages = compiler.FormatDiagnostics();
