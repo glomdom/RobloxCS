b975d2693da8384f569a45d470a1d7cc4a7adeec
glomdom <103685817+glomdom@users.noreply.github.com>
2026-08-05 17:05:29 +0300

    feat: golden file for transpiler rewrite

---
+++ RobloxCS.Tests/Data/StaticTest.client.cs
@@ -0,0 +1,26 @@
+﻿using RobloxCS.Types.Attributes;
+
+namespace RobloxCS.Tests.Data;
+
+internal class StaticTest {
+    [EntryPoint]
+    internal void Main() {
+        var greeting = "slice";
+        int a = 1, b = 2;
+        int c;
+
+        c = 3;
+
+        Console.WriteLine(greeting);
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+
+        Echo(42);
+        Echo(c);
+    }
+
+    private static void Echo(int value) {
+        Console.WriteLine(value);
+    }
+}
