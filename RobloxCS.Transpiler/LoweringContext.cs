4feffa82a022d87d0b32f2924fec9c125a7f2146
glomdom <103685817+glomdom@users.noreply.github.com>
2026-08-05 17:11:55 +0300

    feat: design new `LoweringContext`, work with it

---
+++ RobloxCS.Transpiler/LoweringContext.cs
@@ -0,0 +1,25 @@
+﻿using RobloxCS.AST.Statements;
+
+namespace RobloxCS.Transpiler;
+
+public sealed class LoweringContext {
+    public List<Statement> Prologue { get; } = [];
+
+    private readonly Stack<List<Statement>> _buffers = new();
+
+    public void Emit(Statement stmt) => _buffers.Peek().Add(stmt);
+
+    public List<Statement> Capture(Action body) {
+        _buffers.Push([]);
+
+        try {
+            body();
+
+            return _buffers.Pop();
+        } catch {
+            _buffers.Pop();
+
+            throw;
+        }
+    }
+}
