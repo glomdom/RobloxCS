2c8b6f368100d4ba9c5d2cc4acaac7e07e8d2295
glomdom <103685817+glomdom@users.noreply.github.com>
2026-08-06 18:36:22 +0300

    fix(hir): require

--- RobloxCS.HIR/Expressions/HirArgument.cs
+++ RobloxCS.HIR/Expressions/HirArgument.cs
@@ -3,6 +3,6 @@
 namespace RobloxCS.HIR.Expressions;
 
 public sealed record HirArgument : HirExpression {
-    public IParameterSymbol Symbol { get; init; }
+    public required IParameterSymbol Symbol { get; init; }
     public required HirExpression Value { get; init; }
 }
